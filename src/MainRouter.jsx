6b1f9df676b292808c8767f1567e3d46dabb164a
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-11-23 13:30:01 -0500

    Changes have been made

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import Projects from "./components/projects";
 import Home from "./components/Home";
+import About from "./components/About";
 import NotFound from "./components/NotFound";
 import Layout from "./components/Layout";
+import Services from "./components/Services";
 import ListInventory from "./components/inventory/ListInventory";
 import AddInventory from "./components/inventory/AddInventory";
 import EditInventory from "./components/inventory/EditInventory";
-import Signin from "./components/auth/Signin";
-import Signup from "./components/auth/Signup";
+import SignIn from "./components/auth/Signin";
+import SignUp from "./components/auth/Signup";
 
 function MainRouter() {
     return (
@@ -14,11 +17,14 @@ function MainRouter() {
             <Layout />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/users/signin" element={<Signin />} />
-                <Route path="/users/signup" element={<Signup />} />
+                <Route path="/about" element={<About />} />
+                {/* <Route path="/projects" element={<Projects />} />
+                <Route path="/services" element={<Services />} />
                 <Route path="/inventory/list" element={<ListInventory />} />
                 <Route path="/inventory/add" element={<AddInventory />} />
-                <Route path="/inventory/edit/:id" element={<EditInventory />} />
+                <Route path="/inventory/edit/:id" element={<EditInventory />} /> */}
+                <Route path="/users/signin" element={<SignIn />} />
+                <Route path="/users/signup" element={<SignUp />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
