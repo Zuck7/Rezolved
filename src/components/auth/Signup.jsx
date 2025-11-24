e52b37202d1214395269a8486e46c5f2da05cc76
jrsmistica <janrobi.mistica.na@gmail.com>
2025-11-23 23:36:23 -0500

    Update Signup.jsx

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -41,34 +41,11 @@ const Signup = () => {
         <div className="container" style={{ paddingTop: 10 }}>
             <div className="row">
                 <div className="offset-md-3 col-md-6">
-                    <h1>Add a new user</h1>
+                    <h1>Sign Up to LeadGlobe</h1>
                     <p className="flash"><span>{errorMsg}</span></p>
                     <form onSubmit={handleSubmit} className="form card p-3">
                         <div className="form-group">
-                            <label htmlFor="firstNameTextField">First Name</label>
-                            <input type="text" className="form-control"
-                                id="firstNameTextField"
-                                placeholder="Enter first name"
-                                name="firstName"
-                                value={user.firstName || ''}
-                                onChange={handleChange}
-                                required>
-                            </input>
-                        </div>
-                        <br />
-                        <div className="form-group">
-                            <label htmlFor="lastNameTextField">Last Name</label>
-                            <input type="text" className="form-control"
-                                id="lastNameTextField"
-                                placeholder="Enter last name"
-                                name="lastName"
-                                value={user.lastName || ''}
-                                onChange={handleChange}>
-                            </input>
-                        </div>
-                        <br />
-                        <div className="form-group">
-                            <label htmlFor="usernameTextField">sername</label>
+                            <label htmlFor="usernameTextField">Username</label>
                             <input type="text" className="form-control"
                                 id="usernameTextField"
                                 placeholder="Enter username"
@@ -115,7 +92,7 @@ const Signup = () => {
                         &nbsp;
                         <button className="btn btn-primary" type="submit">
                             <i className="fas fa-edit"></i>
-                            Submit
+                            Register
                         </button>
                         &nbsp; &nbsp;
                         <Link href="#" to="/users/signin" className="btn btn-warning">
