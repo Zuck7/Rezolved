c63c0d91ddc90e0322edaa332ff40ab3faec4620
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-11-27 19:03:54 -0500

    Test if works

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,105 +1,36 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import image_logo from "../assets/logo.png"
+import { Link, useLocation } from 'react-router-dom';
+import image_logo from "../assets/image_logo.png"
 import { isAuthenticated, getUsername, clearJWT } from './auth/auth-helper';
 
 function Layout() {
-    const navigate = useNavigate();
-    const jwt = auth.isAuthenticated();
-    const user = auth.getUser(); // { id, username }
-
-    const signoutClick = () => {
-        auth.clear(() => navigate("/signin"));
-    };
 
     const location = useLocation();
-    const navigate = useNavigate();
 
     const signoutClick = () => {
         clearJWT();
-        navigate('/');
     }
 
     return (
         <>
-            <h1>LeadGlobe Help Desk</h1>
-
+            <h1>LeadGlobe Ticket Management</h1>
             <nav className="navbar">
-                <img src={image_logo} alt="Logo" className="logo" />
-
-                {/* PUBLIC LINKS */}
+                <img src={image_logo} alt="Logo" className='logo' />
                 <Link to="/">
                     <i className="fas fa-home"></i> Home
                 </Link>
-                {/* <Link to="/about">
-                    <i className="fa-solid fa-address-card"></i> About
-                </Link>
-
-                <Link to="/projects">
-                    <i className="fas fa-project-diagram"></i> Projects
-                </Link>
-
-                <Link to="/services">
-                    Services
-                </Link>
-
-                {/* ONLY SHOW WHEN LOGGED IN */}
-                {jwt && (
-                    <>
-                        {/* TICKETS */}
-                        <Link to="/tickets">
-                            Tickets
-                        </Link>
-
-                        <Link to="/tickets/new">
-                            New Ticket
-                        </Link>
-
-                        {/* EVENTS */}
-                        <Link to="/events">
-                            Events
-                        </Link>
-
-                {isAuthenticated() && (
-                    <>
-                        <Link to="/tickets">
-                            Tickets
-                        </Link>
-                        <Link to="/tickets/new">
-                            New Ticket
-                        </Link>
-                        <Link to="/events">
-                            Events
-                        </Link>
-                        <Link to="/events/new">
-                            New Event
-                        </Link>
-                    </>
-                )}
-
-                {/* AUTH LINKS */}
-                {!jwt && (
-                    <>
-                        <Link to="/signin">
-                            <i className="fa-solid fa-right-to-bracket"></i> Signin
-                        </Link>
-
-                        <Link to="/signup">
-                            <i className="fa-solid fa-user-plus"></i> Signup
-                        </Link>
-                    </>
-                )}
+                {isAuthenticated() &&
+                <Link to="/ticket/list">
+                    <i className="fa-regular fa-rectangle-list"></i>Ticket List
+                </Link>}
                 {!isAuthenticated() &&
                     <Link to="/users/signin">
-                        <i className="fa-solid fa-right-to-bracket"></i> Signin
+                        <i className="fa-solid fa-right-to-bracket"></i> Register/Login
                     </Link>}
                 {isAuthenticated() &&
                     <Link to="/" onClick={signoutClick}>
-                        <i className="fa-solid fa-right-from-bracket"></i>
-                        Sign-out ({user?.username})
-                    </Link>
-                }
+                        <i className="fa-solid fa-right-from-bracket"></i> Sign-out ({getUsername()})
+                    </Link>}
             </nav>
-
             <br />
             <hr />
         </>
