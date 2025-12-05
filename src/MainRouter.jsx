3b9d28f4dc99de2ee5047f8f49af95d421bbfd6d
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-12-04 19:16:16 -0500

    Commit for me

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -2,17 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import Layout from "./components/Layout";
-<<<<<<< HEAD
-
-
-import ListEvent from "./components/events/ListEvent";
-import AddEvent from "./components/events/AddEvent";
-import EditEvent from "./components/events/EditEvent";
-
-import SignIn from "./components/auth/Signin";
-import SignUp from "./components/auth/Signup";
-
-=======
 import ListInventory from "./components/inventory/ListInventory";
 import AddInventory from "./components/inventory/AddInventory";
 import EditInventory from "./components/inventory/EditInventory";
@@ -24,7 +13,6 @@ import SignUp from "./components/auth/Signup";
 
 import PrivateRoute from "./components/auth/PrivateRoute";
 
->>>>>>> 6ed1afe7c8a994dffb48971db7c675e7536c412f
 function MainRouter() {
     return (
         <div>
@@ -32,73 +20,6 @@ function MainRouter() {
             <Layout />
 
             <Routes>
-<<<<<<< HEAD
-
-                {/* PUBLIC ROUTES */}
-                <Route path="/" element={<Home />} />
-
-                {/* TICKET ROUTES */}
-                <Route
-                    path="/tickets"
-                    element={
-                        <PrivateRoute>
-                            <ListInventory />
-                        </PrivateRoute>
-                    }
-                />
-
-                <Route
-                    path="/tickets/new"
-                    element={
-                        <PrivateRoute>
-                            <AddInventory />
-                        </PrivateRoute>
-                    }
-                />
-
-                <Route
-                    path="/tickets/edit/:id"
-                    element={
-                        <PrivateRoute>
-                            <EditInventory />
-                        </PrivateRoute>
-                    }
-                />
-
-                {/* EVENT ROUTES */}
-                <Route
-                    path="/events"
-                    element={
-                        <PrivateRoute>
-                            <ListEvents />
-                        </PrivateRoute>
-                    }
-                />
-
-                <Route
-                    path="/events/new"
-                    element={
-                        <PrivateRoute>
-                            <AddEvent />
-                        </PrivateRoute>
-                    }
-                />
-
-                <Route
-                    path="/events/edit/:id"
-                    element={
-                        <PrivateRoute>
-                            <EditEvent />
-                        </PrivateRoute>
-                    }
-                />
-
-                {/* 404 ROUTE */}
-                <Route path="/inventory/list" element={<ListInventory />} />
-                <Route path="/inventory/add" element={<AddInventory />} />
-                <Route path="/inventory/edit/:id" element={<EditInventory />} />
-=======
->>>>>>> 6ed1afe7c8a994dffb48971db7c675e7536c412f
                 {/* PUBLIC ROUTES */}
                 <Route path="/" element={<Home />} />
                 <Route path="/users/signin" element={<SignIn />} />
